c62d51d6423d11a94ab3cd7be7c463cbc7ec2a35
mujohn26 <mujohn26@gmail.com>
2020-09-16 13:47:12 +0200

    - Adding components,configurations and styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./App";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import { IConfig } from './config/interfaces';
+import { Context } from './context/context';
+import './stylesheets/index.css';
 
-render(<App />, document.body);
+window.addEventListener('DOMContentLoaded', (event) => {
+ window.parent.postMessage(JSON.stringify({ action: 'init' }), '*');
+ window.removeEventListener('DOMContentLoaded', () => null);
+});
+
+window.addEventListener('message', (event) => {
+ event.preventDefault();
+ if (!event.data || (typeof event.data !== 'string')) return;
+ const config: IConfig = JSON.parse(event.data);
+ return render(
+   <Context.Provider value={JSON.stringify(config)}>
+     <App />
+   </Context.Provider>,
+   document.body
+ );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,70 @@
-const { resolve } = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserWebpackPlugin = require("terser-webpack-plugin");
-const isProd = process.env.NODE_ENV === "production";
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const config = {
-  mode: isProd ? "production" : "development",
-  entry: {
-    index: "./src/index.tsx",
-  },
-  output: {
-    path: resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      inject: "body",
-    }),
-  ],
+ mode: isProd ? 'production' : 'development',
+ entry: {
+   app: [
+     'webpack-dev-server/client?http://0.0.0.0:9000/',
+     'webpack/hot/only-dev-server',
+     './src/index.tsx'
+   ],
+   Widget: ['./src/widget.ts']
+ },
+ output: {
+   filename: '[name].js',
+   path: resolve(__dirname, 'dist'),
+   library: '[name]',
+   libraryTarget: 'umd',
+   libraryExport: 'default'
+ },
+ resolve: {
+   extensions: ['.js', '.jsx', '.ts', '.tsx'],
+ },
+ module: {
+   rules: [
+     {
+       test: /\.tsx?$/,
+       use: 'babel-loader',
+       exclude: /node_modules/,
+     },
+     {
+       test: /\.css?$/,
+       use: [
+         'style-loader',
+         { loader: 'css-loader', options: { importLoaders: 1 } },
+         'postcss-loader'
+       ]
+     },
+   ],
+ },
+ plugins: [
+   new HtmlWebpackPlugin({
+     template: './src/index.html',
+     hash: true,
+     filename: 'index.html',
+     inject: 'body',
+     excludeChunks: ['widget']
+   }),
+ ],
 };
 
 if (isProd) {
-  config.optimization = {
-    minimizer: [new TerserWebpackPlugin()],
-  };
+ config.optimization = {
+   minimizer: [new TerserWebpackPlugin(),],
+ };
 } else {
-  config.devServer = {
-    port: 9000,
-    open: true,
-    hot: true,
-    compress: true,
-    stats: "errors-only",
-    overlay: true,
-  };
+ config.devServer = {
+   port: 9000,
+   open: true,
+   hot: true,
+   compress: true,
+   stats: 'errors-only',
+   overlay: true,
+ };
 }
 
 module.exports = config;
